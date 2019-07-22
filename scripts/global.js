958d84faa42f88ede3964d964d71122b23231cef
mrsKaneshiro <360551737@qq.com>
2019-07-22 16:25:46 +0800

    增加了一个table

--- scripts/global.js
+++ scripts/global.js
@@ -7,17 +7,29 @@ function preparePlaceholder(){
     p.setAttribute("id","description");
     var text=document.createTextNode("choose a picture");
     p.appendChild(text);
-    var ul=document.getElementById("imagegallery");
-    insertAfter(img,ul);
-    insertAfter(p,img);
+    try{
+        var ul=document.getElementById("imagegallery");
+        insertAfter(img,ul);
+        console.log("img")
+        insertAfter(p,img);
+        console.log("p")
+    }catch (e) {
+        var ul=document.querySelector("ul")
+    }
 }
 
 function prepareGallery(){
     if(!document.getElementsByTagName) return false;
     if(!document.getElementById) return false;
+    try{
+        var as=document.getElementById("imagegallery");
+        var links=as.getElementsByTagName("a");
+    }catch (e) {
+        var as=document.querySelector("ul")
+        console.log(as);
+        var links=as.getElementsByTagName("a");
+    }
 
-    var as=document.getElementById("imagegallery");
-    var links=as.getElementsByTagName("a");
     for (var i=0;i<links.length;i++){
         links[i].onmouseover=function()
         {
@@ -58,7 +70,7 @@ function addLoadEvent(func) {
 }
 
 function insertAfter(newElement,targetElement) {
-    var parent=targetElement.parentNode;
+    var parent=targetElement.parentElement;
     if(parent.lastChild==targetElement){
         parent.appendChild(newElement);
     }else {
@@ -119,7 +131,69 @@ function showSection(id) {
     }
 
 }
+function displayAbbreviations() {
+    /*
+    用于动态的把<attr>中title展示出来
+    但是没有修改样式
+     */
+    var attr=document.querySelectorAll("attr");
+    var attr_title=new Array();
+    var attr_key=new Array();
+    var div=document.createElement("div");
+    div.innerText="";
+    for (var i=0;i<attr.length;i++){
+        attr_title[i]=attr[i].getAttribute("title");
+        attr_key[i]=attr[i].innerHTML;
+        var temp=attr_key[i]+" "+attr_title[i];
+        div.innerText+="    "+temp;
+    }
+    var h3=document.createElement("h3");
+    h3.innerText="Abbreviations"
+    try{
+        var article=document.getElementById("live_article");
+        article.appendChild(h3);
+        article.appendChild(div);
+    }catch (e) {
+        return
+    }
+}
+function stripe() {
+    /*
+    颜色不知道为什么没改过来？
+    中间一条的背景色应该是白色；
+     */
+    if(!document.getElementsByTagName) return false
+    var table=document.getElementsByTagName("table");
+    var odd,row;
+    for(var i=0;i<table.length;i++){
+        odd=false;
+        rows=document.getElementsByTagName("tr");
+        for (var j=0;j<rows.length;j++){
+            if(odd==true){
+                odd=false;
+            }
+            else {
+                rows[j].style.backgroundColor='white';
+                odd=true;
+            }
+        }
+    }
+}
+function highlight () {
+    var row=document.getElementsByTagName("tr");
+    for (var j=0;j<row.length;j++){
+        row[j].onmouseover=function () {
+            this.style.fontWeight = "bold";
+        }
+        row[j].onmouseout=function () {
+            this.style.fontWeight="normal";
+        }
+    }
+}
 addLoadEvent(highLightPage);
+addLoadEvent(stripe);
 addLoadEvent(prepareInternalnav);
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
+addLoadEvent(highlight);
+addLoadEvent(displayAbbreviations) ;
